ebd404e541d74be9cbbdbe30a5d4e3b560c44692
mopsyshin <mopsyshin@gmail.com>
2018-07-05 01:20:54 +0900

    api supertest added

--- index.js
+++ index.js
@@ -10,8 +10,19 @@ const users = [
 app.use(morgan('dev'));
 
 app.get('/users', function(req, res) {
-  res.json(users);
+  req.query.limit = req.query.limit || 10;
+  const limit = parseInt(req.query.limit, 10);
+  if (Number.isNaN(limit)) {
+    return res.status(400).end();
+  }
+  res.json(users.slice(0, limit));
 });
+
+app.get('/users/:id', function(req, res) {
+  const id = parseInt(req.params.id, 10);
+  const user = users.filter((user) => user.id === id )[0];
+  res.json(user);
+})
 
 app.listen(3000, function() {
   console.log("Example app listening on port 3000");
